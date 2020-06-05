af8cce471afb6eef372243f84facc70b8218168a
Angi13 <angkry000@utp.edu.pl>
2020-06-05 16:02:24 +0200

    commit

--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -84,10 +84,26 @@ public class PacMan : MonoBehaviour {
 
         if(targetNode!=currentNode && targetNode != null)
         {
+            if(nextDirection == direction * -1)
+            {
+                direction *= -1;
+
+                Node tempNode = targetNode;
+                targetNode = previousNode;
+                previousNode = tempNode;
+            }
+            
             if(OverShotTarget())
             {
                 currentNode = targetNode;
                 transform.localPosition = currentNode.transform.position;
+                GameObject otherPortal = GetPortal(currentNode.transform.position);
+
+                if(otherPortal != null)
+                {
+                    transform.localPosition = otherPortal.transform.position;
+                    currentNode = otherPortal.GetComponent<Node>();
+                }
                 Node moveToNode = CanMove(nextDirection);
                 if (moveToNode != null)
                     direction = nextDirection;
@@ -194,4 +210,21 @@ public class PacMan : MonoBehaviour {
         Vector2 vec = targetPosition - (Vector2)previousNode.transform.position;
         return vec.sqrMagnitude;
     }
+    GameObject GetPortal(Vector2 pos)
+    {
+        GameObject tile = GameObject.Find("Game").GetComponent<GameBoard>().board[(int)pos.x, (int)pos.y];
+        if (tile != null)
+        {
+            if (tile.GetComponent<Tile>() != null)
+            {
+                if (tile.GetComponent<Tile>().isPortal)
+                {
+                    GameObject otherPortal = tile.GetComponent<Tile>().portalReciver;
+                    return otherPortal;
+                }
+            }
+        }
+        return null;
+    }
 }
+
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour {
 
+    public bool isPortal;
+    public GameObject portalReciver;
+
 	// Use this for initialization
 	void Start () {
 		
